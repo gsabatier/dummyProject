8d5903b0a52604aeaf36f7a2c904210e6bc46fc8
Pranav PANDEY <pranav8494@gmail.com>
2015-08-31 14:32:57 +0200

    Fixing stupid mistakes

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -5,6 +5,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 
 		Greeter greeter = new Greeter();
-		System.out.println(greeter.sayHello() + "@" + greeter.getGreeterTime());
+		System.out.println(greeter.sayHello());
 	}
 }
