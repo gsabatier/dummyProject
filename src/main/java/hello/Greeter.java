ff2a3c18fd06750e94a89bb613b6f57b7fd9d3b0
unknown <gsabatier@NCEL26254.iis.amadeus.net>
2015-09-25 16:42:23 +0200

    getting ready for CI

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -1,22 +1,22 @@
 package hello;
 
-//import org.joda.time.DateTime;
+import org.joda.time.DateTime;
 
 public class Greeter {
 
-//	private DateTime currentTime = new DateTime();
-	
+	private DateTime currentTime = new DateTime();
+
 	public String sayHello() {
 
 		String result = "Hello World";
-//		result += " @ " + getGreeterTime();
+		result += " @ " + getGreeterTime();
 		return result;
 	}
-	
-/*
+
+
 	public DateTime getGreeterTime() {
 
 		return this.currentTime;
 	}
-*/
+
 }
